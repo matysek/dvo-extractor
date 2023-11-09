0f41172072dbe7d42084d6c5ec2b709a04865998
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-11-09 07:40:07 +0100

    Python stub

---
+++ dvo_extractor/command_line.py
@@ -0,0 +1,55 @@
+# Copyright 2023 Red Hat Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""command_line submodule includes the handlers for CLI commands."""
+
+import argparse
+import logging
+import sys
+import os
+
+
+def parse_args():
+    """Parse the command line options and arguments."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("config", nargs="?", help="Application Configuration.")
+    parser.add_argument("--version", help="Show version", action="store_true")
+    return parser.parse_args()
+
+
+def print_version():
+    """Log version information."""
+    logger = logging.getLogger(__name__)
+    logger.info(
+        "Python interpreter version: %d.%d.%d",
+        sys.version_info.major,
+        sys.version_info.minor,
+        sys.version_info.micro,
+    )
+    logger.info(
+        "dvo-extractor version: %s",
+        pkg_resources.get_distribution("dvo-extractor").version,
+    )
+
+
+def insights_sha_extractor():
+    """Handle for dvo-extractor command."""
+    args = parse_args()
+
+    if args.version:
+        logging.basicConfig(format="%(message)s", level=logging.INFO)
+        print_version()
+        sys.exit(0)
+
+    sys.exit(1)
