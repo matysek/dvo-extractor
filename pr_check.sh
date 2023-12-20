f368db77c08cf4def316fc81419d44ab35e8510b
José Luis Segura Lucas <joseluis@redhat.com>
2023-12-20 10:02:55 +0100

    Adding pr_check

--- pr_check.sh
+++ pr_check.sh
@@ -1,29 +1,80 @@
 #!/bin/bash
+# Copyright 2023 Red Hat, Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 # --------------------------------------------
 # Options that must be configured by app owner
 # --------------------------------------------
-export APP_NAME="ccx-dvo-extractor"  # name of app-sre "application" folder this component lives in
-export COMPONENT_NAME="ccx-dvo-extractor"
-export IMAGE="quay.io/cloudservices/ccx-dvo-extractor"
+APP_NAME="ccx-data-pipeline"  # name of app-sre "application" folder this component lives in
+COMPONENT_NAME="dvo-extractor"
+IMAGE="quay.io/cloudservices/dvo-extractor"
+COMPONENTS="dvo-extractor"  # space-separated list of components to laod
+COMPONENTS_W_RESOURCES="dvo-extractor"  # component to keep
+CACHE_FROM_LATEST_IMAGE="true"
 
 export IQE_PLUGINS="ccx"
-export IQE_MARKER_EXPRESSION="smoke"
-export IQE_FILTER_EXPRESSION=""
+export IQE_MARKER_EXPRESSION=""
+# There are no tests for this service yet, just checking deployability
+export IQE_FILTER_EXPRESSION="test_plugin_accessible"
+export IQE_REQUIREMENTS_PRIORITY=""
+export IQE_TEST_IMPORTANCE=""
 export IQE_CJI_TIMEOUT="30m"
 
-## # Install bonfire repo/initialize
-## CICD_URL=https://raw.githubusercontent.com/RedHatInsights/bonfire/master/cicd
-## curl -s $CICD_URL/bootstrap.sh > .cicd_bootstrap.sh && source .cicd_bootstrap.sh
-## 
-## # Build the image and push to quay
-## source $CICD_ROOT/build.sh
-## 
-## # Run the unit tests with an ephemeral db
-## # source $APP_ROOT/unit_test.sh
-## 
-## # Deploy rbac to an ephemeral namespace for testing
-## source $CICD_ROOT/deploy_ephemeral_env.sh
-## 
-## # Run smoke tests with ClowdJobInvocation
-## source $CICD_ROOT/cji_smoke_test.sh
+function changes_including_ocp_rules_version() {
+    git log -1 HEAD . | grep "Bumped ccx-rules-ocp version"
+}
+
+function create_junit_dummy_result() {
+    mkdir -p 'artifacts'
+
+    cat <<- EOF > 'artifacts/junit-dummy.xml'
+	<?xml version="1.0" encoding="UTF-8"?>
+	<testsuite tests="1">
+	    <testcase classname="dummy" name="dummy-empty-test"/>
+	</testsuite>
+	EOF
+}
+
+function build_image() {
+    source $CICD_ROOT/build.sh
+}
+
+function deploy_ephemeral() {
+    source $CICD_ROOT/deploy_ephemeral_env.sh
+}
+
+function run_smoke_tests() {
+    source $CICD_ROOT/cji_smoke_test.sh
+    source $CICD_ROOT/post_test_results.sh  # publish results in Ibutsu
+}
+
+
+# for ccx-rules-ocp version bump PRs skip pr_check.sh tests.
+if changes_including_ocp_rules_version; then
+    echo "Only ccx-rules-ocp version bump, exiting"
+    create_junit_dummy_result
+    exit 0
+fi
+
+# Install bonfire repo/initialize
+CICD_URL=https://raw.githubusercontent.com/RedHatInsights/bonfire/master/cicd
+curl -s $CICD_URL/bootstrap.sh > .cicd_bootstrap.sh && source .cicd_bootstrap.sh
+echo "creating PR image"
+build_image
+
+echo "deploying to ephemeral"
+deploy_ephemeral
+
+echo "running PR smoke tests"
+run_smoke_tests
